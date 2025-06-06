dc5b9ffeb2c71a3e26212c990753144a6df68a81
BalajiLeninrajan <bleninra@uwaterloo.ca>
2025-06-06 12:34:54 -0400

    added neon postgres

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,13 +1,4 @@
-// import mysql from "mysql2/promise";
-//
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-//
-// export default pool;
+import { neon } from "@neondatabase/serverless";
+import { env } from "../env.js";
+
+export const sql = neon(env.DATABASE_URL);
