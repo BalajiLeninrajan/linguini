a6230b6bc1cbb7eccd371d5236d06c16ebc1c5e3
BalajiLeninrajan <bleninra@uwaterloo.ca>
2025-06-18 16:57:39 -0400

    removed unused imports

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -8,7 +8,6 @@ import {
 } from "~/server/api/trpc";
 import { sql, type DBUser } from "~/server/db";
 import { signJWT } from "~/server/utils/jwt";
-import type { User } from "~/types";
 
 export const authRouter = createTRPCRouter({
   /**
