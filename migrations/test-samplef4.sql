2bc614afe99c5f2d64dd8b4b8c0c59797861424c
JuliaIlioukhina001 <jilioukhina@gmail.com>
2025-06-19 12:16:38 -0400

    minor f4 fix

--- migrations/test-samplef4.sql
+++ migrations/test-samplef4.sql
@@ -1,3 +1,5 @@
 INSERT INTO Groups (name, created_at) VALUES
 ('GroupTheBest', '2025-06-18 11:29:00');
+INSERT INTO groupMembers (group_id, user_id, is_owner) VALUES
+(‘1’, ‘1’, true);
 SELECT * FROM Groups;
