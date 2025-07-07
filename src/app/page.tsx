d7337c326e3870ec0a6bc8caf0a3a24ae0f0eaaa
mars273 <mturetsk@uwaterloo.ca>
2025-07-07 17:02:53 -0400

    fixed changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
+import SignupPage from "./_components/signup/page";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -9,9 +10,11 @@ export default async function Home() {
   void api.post.getLatest.prefetch();
 
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+    <div className="min-h-screen bg-[#FFF1D4]">
+      <SignupPage />
+      {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"> */}
+
+        {/* <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
@@ -46,8 +49,8 @@ export default async function Home() {
           </div>
 
           <LatestPost />
-        </div>
-      </main>
-    </HydrateClient>
+        </div> */}
+      {/* </main> */}
+    </div>
   );
 }
