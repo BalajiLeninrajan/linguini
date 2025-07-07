28793f7fbf3f701aa665e0e7e8f6d949021f5b93
mars273 <mturetsk@uwaterloo.ca>
2025-07-07 19:46:25 -0400

    changed styles

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 export default function Header(){
 
     return(
-        <div className='flex justify-center p-8 absolute z-5 w-full' style={{ backgroundColor: '#F6CF81' }}>
-            <Image src='/linguini.svg' alt='Linguini' width={200} height={200}/>
-            <Image src='/pasta.svg' alt='Linguini' width={300} height={200} className='absolute left-0 top-29'/>
+        <div className='flex justify-center p-6 absolute z-5 w-full' style={{ backgroundColor: '#F6CF81' }}>
+            <Image src='/linguini.svg' alt='Linguini' width={250} height={250}/>
+            <Image src='/pasta.svg' alt='Linguini' width={300} height={200} className='absolute left-0 top-28'/>
         </div>
     )
 }
