4866169e359fd01f7f171e7e33f78b57a4b3cd74
mars273 <mturetsk@uwaterloo.ca>
2025-07-07 18:45:13 -0400

    created global leaderboard

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,77 @@
+'use client'
+import {useState, useEffect} from 'react';
+import { Card, CardHeader, CardTitle, CardDescription, CardAction, CardFooter, CardContent } from '~/components/ui/card';
+import { Input } from "~/components/ui/input"
+import { Label } from '~/components/ui/label';
+import { Button } from '~/components/ui/button';
+import SignupPage from '../signup/page';
+import Link from 'next/link';
+
+export default function Leaderboard() {
+    //hardcoding UI values for testing
+    const [users, setUsers] = useState([
+            {
+                ranking: 1,
+                username: "Alex",
+                categoryCount: 3,
+                time: 53
+            },
+            {
+                ranking: 2,
+                username: "Julia",
+                categoryCount: 3,
+                time: 53
+            },
+            {
+                ranking: 3,
+                username: "Balaji",
+                categoryCount: 3,
+                time: 53
+            },
+            {
+                ranking: 4,
+                username: "Nicolas",
+                categoryCount: 3,
+                time: 53
+            },
+            {
+                ranking: 5,
+                username: "George",
+                categoryCount: 3,
+                time: 53
+            }
+        ]);
+
+    return(
+        <div className='min-h-screen flex items-center justify-center bg-[#FFF1D4]'>
+            <div className='w-1/3'>
+                <Card className="w-full">
+                <CardHeader>
+                    <CardTitle className='text-yellow-600 text-5xl'>Global Leaderboard</CardTitle>
+                </CardHeader>
+        
+                {users.map((value, key) => (
+                    <div key={key} className='flex flex-row justify-between w-full bg-white rounded-full px-4 py-4 -mb-2'>
+                        <div className='flex flex-row justify-between w-1/4'>
+                            <p className='text-yellow-600 text-xl font-bold'>{value.ranking}</p>
+                            <p className='ml-auto text-left w-1/2 text-xl text-gray-600'>{value.username}</p>
+                        </div>
+                        <div className='flex flex-row justify-between'>
+                            <p className='text-xl font-bold text-amber-700'>{value.categoryCount} ct.</p>
+                            <p className='ml-8 text-xl font-bold text-amber-700'>{value.time}s</p>
+                        </div>
+                    </div>
+                ))}
+
+
+                </Card>
+                
+                <div className='flex justify-center text-yellow-600 mt-6'>
+                    <p className='text-3xl font-light'>Checkout out <span className='font-bold cursor-pointer font-bold'>
+                        <Link href='/friend-leaderboard'>Friend Leaderboard</Link>
+                    </span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
