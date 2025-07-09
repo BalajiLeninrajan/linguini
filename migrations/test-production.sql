75e130e57e81eb0fc6f011547c6a74bc358ec29a
Jane <janezeng45@gmail.com>
2025-07-08 22:57:20 -0400

    added production test data (#81)

--- migrations/test-production.sql
+++ migrations/test-production.sql
@@ -1,10 +1,13 @@
 -- User sign up
-SELECT id FROM users WHERE email = $1 OR username = $2;
-INSERT INTO users (email, username, password) VALUES ($1, $2, $3);
+SELECT id FROM users WHERE email = 'user5552@gmail.com' OR username = 'user5552';
+INSERT INTO users (email, username, password) VALUES ('user5552@gmail.com', 'user5552', '1h38uhd743k');
 
 -- User login
-SELECT id, password FROM users WHERE email = $1 OR username = $2;
+SELECT id, password FROM Users WHERE (username='user5552' OR email='user5552@gmail.com');
 
 -- Group Create
-INSERT INTO groups (name) VALUES ($1);
-INSERT INTO group_users (group_id, user_id, is_owner) VALUES ($1, $2, TRUE);
+INSERT INTO groups (name) VALUES ('GroupTheBest');
+INSERT INTO group_users (group_id, user_id, is_owner) VALUES (1, 1, TRUE);
+
+--Word category
+SELECT * FROM word_categories WHERE category = 'melon' AND word = 'watermelon';
