ae540991b9bcf35782008b27283c80509c245990
Jane <janezeng45@gmail.com>
2025-07-19 17:13:01 -0400

    Words categories api (#84)
    * initial commit of words-categories, not tested yet
    
    * The recursive query of the game - currently allows up to recursion depth of 5
    
    * reformatted
    
    * not handling the seed anymore
    
    * reformatted

---
+++ src/server/api/routers/words-categories.ts
@@ -0,0 +1,75 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { sql, type DBWordCategory, type DBCategory } from "~/server/db";
+
+export const wordsCategoriesRouter = createTRPCRouter({
+  /**
+   * Verify if the word entered by the user matches the category
+   * @param word The word entered by the user
+   * @param category The category that was provided
+   * @throws {TRPCError} If something goes wrong
+   */
+  verify: publicProcedure
+    .input(
+      z.object({
+        word: z.string().min(1, "Word is required"),
+        category: z.string().min(1, "Category is required"),
+      }),
+    )
+    .query(async ({ input }): Promise<boolean> => {
+      const { word, category } = input;
+      try {
+        const response: DBWordCategory[] = await sql`
+                WITH RECURSIVE
+                Search(word, category, depth) AS (
+                    SELECT word, category, 0 from word_categories WHERE word = ${word}
+
+                    UNION
+
+                    SELECT s.word, wc.category, s.depth + 1
+                    FROM search s, word_categories wc
+                    WHERE s.category = wc.word AND s.word != wc.category AND s.depth < 5
+                )
+                SELECT *
+                FROM search
+                WHERE word = ${word} AND category = ${category}
+            `;
+        return response.length != 0;
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw error;
+        }
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An unexpected error occurred",
+        });
+      }
+    }),
+
+  /**
+   * Generate category list from the seed
+   * @throws {TRPCError} If something goes wrong
+   */
+  generateCategoriesList: publicProcedure.query(
+    async (): Promise<DBCategory[]> => {
+      try {
+        const categoriesList: DBCategory[] = await sql`
+            SELECT category
+            FROM categories
+            ORDER BY RANDOM();
+        `;
+        return categoriesList;
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw error;
+        }
+
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An unexpected error occurred",
+        });
+      }
+    },
+  ),
+});
