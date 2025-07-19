ef3e30a68cb6b39338a7fc00dccac43a1b99a322
Mariya Turetska <63456223+mariyatur27@users.noreply.github.com>
2025-07-19 18:25:37 -0400

    Feature/login signup page (#41)
    * fixed changes
    
    * added a login page and added cursor pointer effect to buttons
    
    * added routing between signup and login pages
    
    * created a file for user auth api
    
    * added Balaji's feedback
    
    * connected signup ui to auth api
    
    * connected login ui to auth api
    
    * added sonner and some try-catch loops
    
    * moved login and signup to auth subfolder
    
    * restored app page.tsx
    
    * fixed signup and login pr
    
    * toasting on errors
    
    ---------
    
    Co-authored-by: mars273 <mturetsk@uwaterloo.ca>

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,122 @@
+'use client'
+import {useState} from 'react';
+import { Card, CardHeader, CardTitle, CardContent } from '~/components/ui/card';
+import { Input } from "~/components/ui/input"
+import { Button } from '~/components/ui/button';
+import Link from 'next/link';
+import { api } from '~/trpc/react';
+import { toast } from 'sonner';
+
+export default function SignupPage() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [error, setError] = useState("");
+
+    const currentUser = api.auth.currentUser.useQuery(undefined, {
+        refetchOnWindowFocus: false,
+    });
+
+    const register = api.auth.register.useMutation({
+        onSuccess: async (data) => {
+            localStorage.setItem("token", data.token);
+            await currentUser.refetch();
+        },
+        onError: (error) => {
+            setError(error.message);
+            toast("An error has occured, please try again!");
+        },
+    });
+
+
+    const createAccount = async (e: React.FormEvent<HTMLFormElement>) => {
+        console.log(username, email, password, passwordConfirmation)
+        e.preventDefault();
+        
+        if(passwordConfirmation != password){
+            toast("Your passwords do not match! Please try again.");
+            setPassword("");
+            setPasswordConfirmation("");
+        }else{
+            //call to the API to signup the user
+            try{
+                register.mutate({
+                    email: email,
+                    username: username,
+                    password: password
+                })
+            }catch(e){
+                toast("An error has occured, please try again!");
+                console.log(e);
+            }
+        }
+    }
+
+    return(
+        <div className='min-h-screen flex items-center justify-center min-h-screen bg-[#FFF1D4]'>
+            <div className='w-1/4'>
+                <Card className="w-full">
+                <CardHeader>
+                    <CardTitle>Sign up</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={createAccount} >
+                    <div className="flex flex-col gap-3">
+                        <div className="grid gap-1">
+                        <Input
+                            id="username"
+                            type="text"
+                            placeholder="Username"
+                            required
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        </div>
+                        <div className="grid gap-1">
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="Email"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        </div>
+                        <div className="grid gap-2">
+                        <Input
+                            id="password"
+                            type="password"
+                            placeholder="Password"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        </div>
+                        <div className="grid gap-2">
+                        <Input
+                            id="passwordConfirmation"
+                            type="password"
+                            placeholder="Confirm Password"
+                            required
+                            value={passwordConfirmation}
+                            onChange={(e) => setPasswordConfirmation(e.target.value)}
+                        />
+                        </div>
+                        <Button variant="brownPrimary" type="submit" className="w-full">
+                            Create Account
+                        </Button>
+                    </div>
+                    </form>
+                </CardContent>
+                </Card>
+                
+                <div className='flex justify-center text-yellow-600 mt-6'>
+                    <p className='text-2xl font-normal'>Not a newbie around here? <span className='font-bold cursor-pointer'>
+                        <Link href="login">Log in</Link>
+                    </span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
