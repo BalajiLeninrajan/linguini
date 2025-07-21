e7ae056f234ab0eb58a78e9c02047624455b526b
BalajiLeninrajan <bleninra@uwaterloo.ca>
2025-07-21 15:02:13 -0400

    logout button works

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,17 +2,25 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
-import { Users, Home, BarChart3, Heart } from "lucide-react";
+import { Users, Home, BarChart3, Heart, LogOut } from "lucide-react";
+import { api } from "~/trpc/react";
 
 export default function Header() {
+  const currentUser = api.auth.currentUser.useQuery(undefined, {
+    refetchOnWindowFocus: false,
+  });
+
+  const handleLogout = async () => {
+    localStorage.removeItem("token");
+    await currentUser.refetch();
+  };
+
   return (
     <div className="absolute z-5 flex w-full items-center justify-between bg-[#F6CF81] p-4">
       <div className="flex-1"></div>
 
       <div className="flex items-center">
-        <Link href="/game">
-          <Image src="/linguini.svg" alt="Linguini" width={250} height={250} className="cursor-pointer" />
-        </Link>
+        <Image src="/linguini.svg" alt="Linguini" width={250} height={250} />
         <Image
           src="/pasta.svg"
           alt="Linguini"
@@ -22,28 +30,35 @@ export default function Header() {
         />
       </div>
 
-      <div className="flex flex-1 justify-end gap-3">
-        <Link href="/groups">
-          <Button variant="nav" size="nav">
-            <Users />
+      {currentUser.data ? (
+        <div className="flex flex-1 justify-end gap-3">
+          <Link href="/groups">
+            <Button variant="nav" size="nav">
+              <Users />
+            </Button>
+          </Link>
+          <Link href="/game">
+            <Button variant="nav" size="nav">
+              <Home />
+            </Button>
+          </Link>
+          <Link href="/leaderboard">
+            <Button variant="nav" size="nav">
+              <BarChart3 />
+            </Button>
+          </Link>
+          <Link href="/about">
+            <Button variant="nav" size="nav">
+              <Heart />
+            </Button>
+          </Link>
+          <Button variant="nav" size="nav" onClick={handleLogout}>
+            <LogOut />
           </Button>
-        </Link>
-        <Link href="/game">
-          <Button variant="nav" size="nav">
-            <Home />
-          </Button>
-        </Link>
-        <Link href="/leaderboard">
-          <Button variant="nav" size="nav">
-            <BarChart3 />
-          </Button>
-        </Link>
-        <Link href="/about">
-          <Button variant="nav" size="nav">
-            <Heart />
-          </Button>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div className="flex flex-1 justify-end gap-3"></div> // css breaks without this
+      )}
     </div>
   );
 }
